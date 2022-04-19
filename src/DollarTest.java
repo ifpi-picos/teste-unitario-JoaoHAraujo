8fb19d85d2d2eeaffdebb3d27111d842bb939ec3
João Araújo <capic.2020118tads0045@aluno.ifpi.edu.br>
2022-04-18 23:19:19 -0300

    feat: currency

--- src/DollarTest.java
+++ src/DollarTest.java
@@ -1,21 +1,13 @@
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
 public class DollarTest {
 
     @Test
-    public void testMultiplication() {
+    public void testDollarMultiplication() {
         Money five = Money.dollar(5);
         assertEquals(Money.dollar(10), five.times(2));
         assertEquals(Money.dollar(15), five.times(3));
     }
-
-    @Test
-    public void testEquality() {
-        assertTrue(new Dollar(5).equals(new Dollar(5)));
-        assertFalse(new Dollar(5).equals(new Dollar(6)));
-     }
 }
--- src/Money.java
+++ src/Money.java
@@ -1,5 +1,6 @@
 abstract class Money {
     protected int amount;
+    protected String currency;
 
     public boolean equals(Object object) {
         Money money = (Money) object;
@@ -7,12 +8,21 @@ abstract class Money {
     }
 
     static Money dollar(int amount) {
-        return new Dollar(amount);
+        return new Dollar(amount, "USD");
     }
 
     static Money franc(int amount) {
-        return new Franc(amount);
+        return new Franc(amount, "CHF");
+    }
+
+    Money(int amount, String currency) {
+        this.amount = amount;
+        this.currency = currency;
     }
 
     abstract Money times(int multiplier);
+
+    String currency() {
+        return currency;
+    }
 }
