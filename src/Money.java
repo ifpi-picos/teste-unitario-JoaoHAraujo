ac89a61d0c46968c4c8e9984bcbbb565289092f4
João Araújo <capic.2020118tads0045@aluno.ifpi.edu.br>
2022-04-18 23:38:11 -0300

    feat: comparing Money and Franc

--- src/Money.java
+++ src/Money.java
@@ -1,18 +1,18 @@
-abstract class Money {
+class Money {
     protected int amount;
     protected String currency;
 
     public boolean equals(Object object) {
         Money money = (Money) object;
-        return amount ==  money.amount && getClass().equals(money.getClass());
+        return amount ==  money.amount && currency().equals(money.currency());
     }
 
     static Money dollar(int amount) {
-        return new Dollar(amount, "USD");
+        return new Money(amount, "USD");
     }
 
     static Money franc(int amount) {
-        return new Franc(amount, "CHF");
+        return new Money(amount, "CHF");
     }
 
     Money(int amount, String currency) {
@@ -20,7 +20,9 @@ abstract class Money {
         this.currency = currency;
     }
 
-    abstract Money times(int multiplier);
+    Money times(int multiplier) {
+        return new Money(amount * multiplier, currency);
+    }
 
     String currency() {
         return currency;
--- src/MoneyTest.java
+++ src/MoneyTest.java
@@ -10,17 +10,14 @@ public class MoneyTest {
     public void testEquality() {
         assertTrue(Money.dollar(5).equals(Money.dollar(5)));
         assertFalse(Money.dollar(5).equals(Money.dollar(6)));
-        assertTrue(Money.franc(5).equals(Money.franc(5)));
-        assertFalse(Money.franc(5).equals(Money.franc(6)));
-        assertFalse(Money.franc(5).equals(Money.dollar(5)));
+        assertFalse(Money.franc(5).equals(Money.dollar(6)));
     }
 
-    @Test
-    public void testFrancMultiplication() {
-        Money five = Money.franc(5);
-        assertEquals(Money.franc(10), five.times(2));
-        assertEquals(Money.franc(15), five.times(3));
-    }
+    public void testMultiplication() {
+        Money five = Money.dollar(5);
+        assertEquals(Money.dollar(10), five.times(2));
+        assertEquals(Money.dollar(15), five.times(3));
+     }
 
     @Test
     public void testCurrency() {
